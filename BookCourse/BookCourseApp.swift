45b56d3318417d1645dffca6944dcb5d9bcbc850
cruasanya <98823717+cruasanya@users.noreply.github.com>
2023-06-08 20:38:21 +0300

    Initial Commit

---
+++ BookCourse/BookCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookCourseApp.swift
+//  BookCourse
+//
+//  Created by Александр Новиков on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookCourse/ByeView.swift
@@ -0,0 +1,20 @@
+//
+//  ByeView.swift
+//  BookCourse
+//
+//  Created by Александр Новиков on 08.06.2023.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text("Bye Bye app")
+    }
+}
+
+struct ByeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ByeView()
+    }
+}
